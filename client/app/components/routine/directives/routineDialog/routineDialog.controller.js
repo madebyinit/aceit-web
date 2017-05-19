9ec512b5def91e2e0b04e9f7033c6f0353b09a9a
orel ohayon <orel.ohayon@hibob.io>
2017-05-20 01:29:28 +0300

    Routine dialog

---
+++ client/app/components/routine/directives/routineDialog/routineDialog.controller.js
@@ -0,0 +1,10 @@
+class RoutineDialogController {
+  constructor() {
+    this.name = 'routineDialog';
+    this.inputTitle = 'Bla bla';
+    this.inputPlaceholder = 'place holder';
+  }
+}
+
+RoutineDialogController.$inject = [];
+export default RoutineDialogController;
