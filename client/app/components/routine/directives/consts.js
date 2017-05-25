ee57a29d30f472af7c65fd225a27ab7359cb3dd3
orel ohayon <orel.ohayon@hibob.io>
2017-05-25 15:08:07 +0300

    Fixed

--- client/app/components/routine/directives/consts.js
+++ client/app/components/routine/directives/consts.js
@@ -4,7 +4,8 @@ let consts = {
   defaultConcentrationPhysical:{one:'Close your eyes',two:'Choose from',three:'Enter power word'},
   CONCENTRATION_TYPE:'concentration_type',
   PHYSICAL_TYPE:'physical_type',
-  CONCENTRATION_PHYSICAL_TYPE:'concentration_physical_type'
+  CONCENTRATION_PHYSICAL_TYPE:'concentration_physical_type',
+  sum:{positive:0,concentration:0,physical:0}
 };
 
 export default consts
