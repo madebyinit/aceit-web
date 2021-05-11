7300043124c8d26a0ac8700da4a9fb85bdfe6811
Itay Zagron <zagron@gmail.com>
2021-05-11 11:44:04 +0300

    changed to production

--- client/app/consts.js
+++ client/app/consts.js
@@ -1,8 +1,8 @@
 const CONSTS = {
-    // imageCdn:'https://cdn.aceit-app.com/img/',
-    // videoCdn:'https://cdn.aceit-app.com/video/',
-    videoCdn: 'https://www.aceitapp-demo.com/video/',
-    imageCdn: 'https://www.aceitapp-demo.com/img/',
+    imageCdn:'https://cdn.aceit-app.com/img/',
+    videoCdn:'https://cdn.aceit-app.com/video/',
+    // videoCdn: 'https://www.aceitapp-demo.com/video/',
+    // imageCdn: 'https://www.aceitapp-demo.com/img/',
     // videoCdn:'http://192.168.1.16:3001/video/',
     // imageCdn:'http://192.168.1.16:3001/img/',
 };
